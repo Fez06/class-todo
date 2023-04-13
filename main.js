ee8928f25287c62bf0f21e12f551789f0bec32a6
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-04-13 17:03:45 +0200

    si aggiungono i todo ma non piu' di uno

--- main.js
+++ main.js
@@ -6,37 +6,29 @@ createApp({
             apiUrl: 'server.php',
             todos: [],
             newToDo: ''
-            /*todos: [
-                {
-                    "text": "HTML",
-                    "done": true
-                },
-                {
-                    "text": "CSS",
-                    "done": true
-                },
-                {
-                    "text": "Responsive design",
-                    "done": true
-                },
-                {
-                    "text": "Javascript",
-                    "done": true
-                },
-                {
-                    "text": "PHP",
-                    "done": true
-                },
-                {
-                    "text": "Laravel",
-                    "done": false
-                }
-            ]*/
         }
     },
     methods: {
         getTodos() {
             axios.get(this.apiUrl).then((response) => {
+                console.log(response);
+                this.todos = response.data;
+            })
+        },
+        //metodo per aggiungere nuovi todo
+        addToDo(){
+            console.log(this.newToDo);
+
+            const headers = {
+                'Content-Type': 'multipart/form-data',
+                'Accept': 'application/json'
+            }
+
+            const data = {
+                add: true,
+                todo: this.newToDo
+            };
+            axios.post(this.apiUrl, data, { headers }).then((response) =>{
                 console.log(response);
                 this.todos = response.data;
             })
